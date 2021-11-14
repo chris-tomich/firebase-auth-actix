239cd6f0dcb6911f0b5f2c0c80d32514f3b05bcb
Chris Tomich <chris.tomich@gmail.com>
2021-11-14 15:04:59 +1100

    WIP Created middleware boiler plate

--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,57 @@
+use std::{future::Ready, pin::Pin};
+
+use actix_service::{Service, Transform};
+use actix_web::{dev::{ServiceRequest, ServiceResponse}, http::Error};
+use futures::{Future, future::{ok}};
+
+pub struct FirebaseAuthentication;
+
+impl<S, B> Transform<S, ServiceRequest> for FirebaseAuthentication
+where
+S: Service<ServiceRequest, Response = ServiceResponse<B>, Error = Error>,
+S::Future: 'static,
+B: 'static,
+{
+    type Response = ServiceResponse<B>;
+
+    type Error = Error;
+
+    type Transform = FirebaseAuthenticationMiddleware<S>;
+
+    type InitError = ();
+
+    type Future = Ready<Result<Self::Transform, Self::InitError>>;
+
+    fn new_transform(&self, service: S) -> Self::Future {
+        todo!()
+    }
+}
+
+pub struct FirebaseAuthenticationMiddleware<S> {
+    service: S,
+}
+
+impl<S, B> Service<ServiceRequest> for FirebaseAuthenticationMiddleware<S>
+where
+S: Service<ServiceRequest, Response = ServiceResponse<B>, Error = Error>,
+S::Future: 'static,
+B: 'static,
+{
+    type Response = ServiceResponse<B>;
+    type Error = Error;
+    type Future = Pin<Box<dyn Future<Output = Result<Self::Response, Self::Error>>>>;
+
+    fn poll_ready(&self, ctx: &mut core::task::Context<'_>) -> std::task::Poll<Result<(), Self::Error>> {
+        todo!()
+    }
+
+    fn call(&self, req: ServiceRequest) -> Self::Future {
+        todo!()
+    }
+
+    
+}
+
 #[cfg(test)]
 mod tests {
     #[test]
